4e4b1f5806e2a71d5504b5ee32100365ff0c4f50
kter <me@kter.jp>
2016-01-09 18:07:42 +0900

    user

--- cookbooks/common-packages/default.rb
+++ cookbooks/common-packages/default.rb
@@ -25,3 +25,43 @@ end
 service "ntpd" do
   action :enable
 end
+
+group "kter" do
+  action :create
+  groupname "kter"
+  gid 1379
+end
+
+user "kter" do
+  username "kter"
+  gid 1379
+  uid 1379
+  shell '/bin/zsh'
+  home '/home/kter'
+  create_home true
+  action :create
+end
+
+directory "/home/kter/.ssh" do
+  action :create
+  owner "kter"
+  group "kter"
+  path "/home/kter/.ssh"
+  mode "700"
+end
+
+remote_file "/home/kter/.ssh/authorized_keys" do
+  owner "kter"
+  group "kter"
+  source "files/authorized_keys"
+end
+
+file "/home/kter/.ssh/authorized_keys" do
+  owner "kter"
+  group "kter"
+  mode "600"
+end
+
+execute "echo '%kter ALL=(ALL) ALL' >> /etc/sudoers" do
+  not_if "grep '%kter ALL=(ALL) ALL' /etc/sudoers"
+end
--- spec/default/sample_spec.rb
+++ spec/default/sample_spec.rb
@@ -23,3 +23,16 @@ describe service('ntpd') do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe user('kter') do
+  it { should exist }
+  it { should belong_to_group 'kter' }
+  it { should have_uid 1379 }
+  it { should have_home_directory '/home/kter' }
+  it { should have_login_shell '/bin/zsh' }
+  it { should have_authorized_key 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCbDlgqZbRItJK6+1jtL7MyeM3/aVeiMQxhrHuP+xkXtbDa9co4U+Bjv9HPPPSmuCxn6gWQygpLE1K0xqUejAiRYbz+FNwGXcXi57g0JjPM3uASCbesAxYdRQ/BSXdktRezWtzvesxwq1Rl9FnoVlhNmSDmpckkfIf5bLo3BgyP5rUUoeB9aMptq1RDpjMooiSGsNoFQISme8SiTmZMSyGBWbfTDquTi9s5imq13XK7rPeBnWBSxYDNUhvvmAm9XrmDgxijzCul1F0/yOOxNCPXUFZM8+3mwGTJzzO7EnPva55Qtmo//gWacifSmCOKE3eepCRNtrQ8U+AosMbz1LrX' }
+end
+
+describe file('/etc/sudoers') do
+  it { should contain '%kter ALL=(ALL) ALL' }
+end
