a1f44ab58be660c99b61a0e3874e50d0721c6e43
kter <me@kter.jp>
2016-01-10 23:31:48 +0900

    itamaeでファイル分けた

---
+++ cookbooks/user/default.rb
@@ -0,0 +1,55 @@
+group "kter" do
+  action :create
+  groupname "kter"
+  gid 1379
+end
+
+user "kter" do
+  username "kter"
+  gid 1379
+  uid 1379
+  shell '/bin/zsh'
+  home '/home/kter'
+  create_home true
+  action :create
+end
+
+directory "/home/kter/.ssh" do
+  action :create
+  owner "kter"
+  group "kter"
+  path "/home/kter/.ssh"
+  mode "700"
+end
+
+remote_file "/home/kter/.ssh/authorized_keys" do
+  owner "kter"
+  group "kter"
+  source "files/authorized_keys"
+end
+
+file "/home/kter/.ssh/authorized_keys" do
+  owner "kter"
+  group "kter"
+  mode "600"
+end
+
+execute "echo '%kter ALL=(ALL) ALL' >> /etc/sudoers" do
+  not_if "grep '%kter ALL=(ALL) ALL' /etc/sudoers"
+end
+
+link "/etc/localtime" do
+  to "/usr/share/zoneinfo/Asia/Tokyo"
+  force true
+end
+
+git "/home/kter/.dotfiles" do
+  # repository "git@github.com:kter/settings.git"
+  repository "https://github.com/kter/settings.git"
+  destination "/home/kter/.dotfiles"
+  user "kter"
+end
+
+execute "/home/kter/.dotfiles/init.sh" do
+  user "kter"
+end
---
+++ roles/default.rb
@@ -0,0 +1,2 @@
+include_recipe "../cookbooks/common-packages/default.rb"
+include_recipe "../cookbooks/user/default.rb"
